c78e9a7513e875a9660df189f7acc9976d57fafb
alumno <massoni_jp@yahoo.com>
2017-03-23 21:34:28 -0300

    commit

---
+++ app/src/main/java/com/example/alumno/mi_aplicacion_clase_2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.alumno.mi_aplicacion_clase_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
